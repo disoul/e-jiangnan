a9482313820db85244f32a0a378762076b5a6aeb
disoul <starky_ds@sina.cn>
2016-03-13 01:45:48 +0800

    init: react-native-router

---
+++ app/components/app.js
@@ -0,0 +1,15 @@
+'use strict';
+import React, {
+  Component,
+  View,
+} from 'react-native';
+
+var Router = require('react-native-router');
+
+export default class App extends Component {
+  render() {
+    return (
+      <View></View>
+    );
+  }
+}
