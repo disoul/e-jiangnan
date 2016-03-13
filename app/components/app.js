ab9ffe987b63468516c52b9a952d49a18ea2656e
disoul <starky_ds@sina.cn>
2016-03-14 01:06:48 +0800

    init: react-native-cookies

--- app/components/app.js
+++ app/components/app.js
@@ -2,14 +2,34 @@
 import React, {
   Component,
   View,
+  TouchableNativeFeedback,
+  Text,
 } from 'react-native';
-
+import { connect } from 'react-redux';
+import { login } from '../actions/actions';
 var Router = require('react-native-router');
 
-export default class App extends Component {
+class App extends Component {
   render() {
+    const { dispatch } = this.props;
     return (
-      <View></View>
+      <View>
+        <TouchableNativeFeedback
+          onPress={() => dispatch(login())}
+          background={TouchableNativeFeedback.Ripple('deeppink', false)}
+          >
+          <View>
+            <Text>Login</Text>
+          </View>
+        </TouchableNativeFeedback>
+      </View>
     );
   }
 }
+
+function select(state) {
+  return {
+  };
+}
+
+export default connect(select)(App);
--- app/reducers/login.js
+++ app/reducers/login.js
@@ -1,9 +1,14 @@
 import { LOGIN_STATE_LOGIN, LOGIN_STATE_LOGOUT } from '../actions/actions'
+var CookieManager = require('react-native-cookies');
 
 export default function login(state, action) {
   switch (action.type) {
     case 'LOGIN':
-      // TODO: do login
+      CookieManager.setFromResponse(
+          'http://e.jiangnan.edu.cn/main/loginIndex.do',
+          'user_session=JSESSIONID; path=/;'
+          (res) => { console.log("Set cookies", res); }
+          );
       return action.state;
     default:
       return LOGIN_STATE_LOGOUT;
